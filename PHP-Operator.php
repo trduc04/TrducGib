638a8a790d67543c3273d799a2eca406ffc2e609
trduc04 <143677073+trduc04@users.noreply.github.com>
2023-12-29 10:16:34 +0700

    Add files via upload

---
+++ PHP-Operator.php
@@ -0,0 +1,18 @@
+
+<?php
+/**
+ * Viết chương trình nhập vào 2 số nguyên a, b:
+*Tính tổng, hiệu, tích, thương của 2 số a, b. In ra màn hình kế t quả. Định dạng 2 chữ số hàng thập phân
+*Khai báo biến c. Gán giá trị biến c cho phép toán so sánh của a với b.
+*Thực thi các toán tử so sánh >, <, =>, >=. Mỗi biểu thức gán xong in kết quả ra màn hình.
+ */
+$a = 10;
+$b = 6;
+ print("Tong a + b = " . ($a + $b));
+ print("<br>");
+ print("Hieu a - b = " . ($a - $b));
+ print("<br>");
+ print("Tich a * b = " . ($a * $b));
+ print("<br>");
+ print("Thuong a / b = " . ($a / $b))
+?>
